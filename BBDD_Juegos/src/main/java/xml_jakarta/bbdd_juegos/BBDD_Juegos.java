b4cd433b0534ebc45d7bf7ba8cfcc54768da3585
TheHoyu <128851507+TheHoyu@users.noreply.github.com>
2023-12-05 10:16:56 +0100

    Update BBDD_Juegos.java

--- BBDD_Juegos/src/main/java/xml_jakarta/bbdd_juegos/BBDD_Juegos.java
+++ BBDD_Juegos/src/main/java/xml_jakarta/bbdd_juegos/BBDD_Juegos.java
@@ -13,32 +13,34 @@ import java.sql.*;
 public class BBDD_Juegos {
         ///jdbc:mysql://localhost:3306/bbdd
         ///jbdc:[tipo]://[IP]:[puerto]/basededatos
-    
-        static final String DB_URL = "jdbc:mysql://localhost:3306/datagaming";
-        static final String USER ="GM";
-        static final String PASS ="1234";
-        static final String QUERY = "SELECT * FROM videojuegos";
+        // datos para lac onexión a la  bbdd 
+        static final String DB_URL = "jdbc:mysql://localhost:3306/datagaming"; // dirección de mi local hosto 
+        static final String USER ="GM"; // usuario
+        static final String PASS ="1234"; // contraseña 
+        static final String QUERY = "SELECT * FROM videojuegos"; // consulta de prueba 
 
         
     public static void main(String[] args) {
 
-      try{Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+      try{Connection conn = DriverManager.getConnection(DB_URL,USER,PASS); // conexión , de la bbdd , la cual usa los parameetros 
+          // introducidos antes , para acceder 
       
-      Statement stmt =  conn.createStatement();
-      ResultSet rs = stmt.executeQuery(QUERY);
+      Statement stmt =  conn.createStatement(); // creamosun Statement (sentencia) 
+          // la cual nos ayuda a procesar una sentecia sql y obtener resultados. 
+      ResultSet rs = stmt.executeQuery(QUERY); // usando el Statemnte -, obtenemos un ResulSet , a traves de nuestra query
       //extraemos la info del set de resultados 
       
-        while(rs.next()){
+        while(rs.next()){// cuando el resultado pase a la siguiente linea , ( porque leealgo ) 
         
                        
                 
             // OBTENEMOS INFO SEGUN EL NOMBRE DE LA COLUMNA
-            System.out.println("Id: "+rs.getInt("id"));
-            System.out.println("Nombre: "+rs.getString("Nombre"));
-            System.out.println("Genero: "+rs.getString("Genero"));
-            System.out.println("Fecha de lanzamiento: "+rs.getDate("FechaLanzamiento"));
-            System.out.println("Compañia: "+rs.getString("Compañia"));
-            System.out.println("Precio: "+rs.getFloat("Precio"));
+            System.out.println("Id: "+rs.getInt("id")); // sacamos la id del juego, con el metodo getInt, obtenemos el id del juego
+            System.out.println("Nombre: "+rs.getString("Nombre"));// sacamos el nombre del juego, con el metodo getString, obtenemos el nombre del juego
+            System.out.println("Genero: "+rs.getString("Genero")); // scamos el genero del juego, con getString, 
+            System.out.println("Fecha de lanzamiento: "+rs.getDate("FechaLanzamiento"));// sacamos la fecha
+            System.out.println("Compañia: "+rs.getString("Compañia"));// sacamos la compañia 
+            System.out.println("Precio: "+rs.getFloat("Precio"));//scamos su precio 
             System.out.println("-------------------------------------------");
             
            
